e02bde37f55f90bbf05c329a2f102e11aa8fa4f7
Ghanashyam Sateesh <ghanashyam74@gmail.com>
2019-11-04 03:39:53 -0800

    Thin Arcs + Geo Accurate

---
+++ 2/src/js/data.js
@@ -0,0 +1,18 @@
+// Convention
+//  N: +ve
+//  E: -ve
+//  S: -ve
+//  W: +ve
+const geoData={
+	china:				{latLong: new Float32Array([ 35.8617,-104.1954])},
+	indonesia:		{latLong: new Float32Array([ -0.7893,-113.9213])},
+	thailand:			{latLong: new Float32Array([ 15.8700,-100.9925])},
+	malaysia:			{latLong: new Float32Array([  4.2105,-101.9758])},
+	usa:					{latLong: new Float32Array([ 37.0902,  95.7129])},
+	singapore:		{latLong: new Float32Array([  1.3521,-103.8198])},
+	canada:				{latLong: new Float32Array([ 56.1304, 106.3468])},
+	philippines:	{latLong: new Float32Array([ 12.8797,-121.7740])},
+	myanmar:			{latLong: new Float32Array([ 21.9162, -95.9560])},
+	vietnam:			{latLong: new Float32Array([ 14.0583,-108.2772])},
+	peru:					{latLong: new Float32Array([ -9.1900,  75.0152])},
+}
--- 2/src/js/setup.js
+++ 2/src/js/setup.js
@@ -25,7 +25,7 @@ window.onresize=()=>setupRenderer()
 const animate=()=>{
 	requestAnimationFrame(animate)
 
-	particleSphere.rotation.y=time/250
+	group.rotation.y=time/250
 
 	renderer.render(scene,camera)
 	++time
@@ -33,4 +33,22 @@ const animate=()=>{
 window.onload=()=>{
 	window.onresize()
 	animate()
+}
+
+const latLongToSpherical=latLong=>new Float32Array([latLong[0]*piBy180,latLong[1]*piBy180])
+const sphericalToCartesian=phiTheta=>{
+	const cartesian=new Float32Array(3)
+
+	const y=Math.sin(phiTheta[0])
+	const radiusAtY=Math.cos(phiTheta[0])
+	cartesian[0]=Math.cos(phiTheta[1])*radiusAtY
+	cartesian[1]=y
+	cartesian[2]=Math.sin(phiTheta[1])*radiusAtY
+	return cartesian
+}
+
+for(var country in geoData){
+	const data=geoData[country]
+	data.phiTheta=latLongToSpherical(data.latLong)
+	data.xyz=sphericalToCartesian(data.phiTheta)
 }
